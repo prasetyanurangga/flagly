d5d029067a3635a5c7062c960b13fdc895289410
prasetyanurangga <prasetyanurangga@users.noreply.github.com>
2022-06-21 06:01:22 +0000

    deploy: 9a331b8ba3ca790e4b12b30369d067eb9f6df675

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "74f5cfb0c9474ab89e7850fac39f1aff",
+  "main.dart.js": "e49cc16d5e7badd3a55ddf27b945ff2c",
 "manifest.json": "799afd514e257eeadd47301c9472d70d",
 "version.json": "1389d0b934e27d4e8a59342f503f9cba",
 "flutter.js": "0816e65a103ba8ba51b174eeeeb2cb67",
-"index.html": "a8584e047281d9a4cfdd9713fb2b4282",
-"/": "a8584e047281d9a4cfdd9713fb2b4282",
+"index.html": "88668db098fe0671272d331ecd050ec5",
+"/": "88668db098fe0671272d331ecd050ec5",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
